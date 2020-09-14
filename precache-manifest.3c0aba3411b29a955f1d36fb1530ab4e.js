e06ca4d227a93ecdf824f3ee7c2767e8ce561bfd
Kartik Tharad <tharadkartik@gmail.com>
2020-09-15 03:05:32 +0530

    Updates

---
+++ precache-manifest.3c0aba3411b29a955f1d36fb1530ab4e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d22982c61bc585102f0bc31a76b467f2",
+    "url": "/Ipod-Using-Reactjs/index.html"
+  },
+  {
+    "revision": "bedfe60d4a24a2be1237",
+    "url": "/Ipod-Using-Reactjs/static/css/2.43079d8c.chunk.css"
+  },
+  {
+    "revision": "895c9cc4424d586e76f6",
+    "url": "/Ipod-Using-Reactjs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bedfe60d4a24a2be1237",
+    "url": "/Ipod-Using-Reactjs/static/js/2.ece0a88d.chunk.js"
+  },
+  {
+    "revision": "3afc3f7b055b07aac8edf61359703488",
+    "url": "/Ipod-Using-Reactjs/static/js/2.ece0a88d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "895c9cc4424d586e76f6",
+    "url": "/Ipod-Using-Reactjs/static/js/main.144fc916.chunk.js"
+  },
+  {
+    "revision": "72c001d045b6425f9109",
+    "url": "/Ipod-Using-Reactjs/static/js/runtime-main.7c553f9b.js"
+  },
+  {
+    "revision": "9ba16188fa31aa053935b27151e30f78",
+    "url": "/Ipod-Using-Reactjs/static/media/Senorita.9ba16188.mp3"
+  },
+  {
+    "revision": "cd3b17af512dfb005115b156d252750b",
+    "url": "/Ipod-Using-Reactjs/static/media/kartik.cd3b17af.jpg"
+  }
+]);
